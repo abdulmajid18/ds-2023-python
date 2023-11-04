3c3c329889159fac24eebc64cb32f19db1c47517
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2023-11-04 00:59:41 +0000

    two pointers

---
+++ leetcode-bloomberg/two_pointer/squares_of_sorted_array.py
@@ -0,0 +1,17 @@
+def squares_of_sorted(nums):
+    res = []
+    l = 0
+    r = len(nums) - 1
+
+    while l < r:
+        if nums[l] * nums[l] > nums[r] * nums[r]:
+            res.append(nums[l] * nums[l])
+            l += 1
+        else:
+            res.append(nums[r] * nums[r])
+            r -= 1
+
+
+
+
+    return res[::1]
