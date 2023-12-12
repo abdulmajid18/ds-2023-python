df02e41203793e99ac512ea826d0bcf6ab0cd2e9
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2023-12-12 07:11:17 +0000

    added swap pairs to linked list

---
+++ ds/linked-list/swap_node_pairs.py
@@ -0,0 +1,32 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def swapPairs(head):
+    # Create a dummy node to simplify edge cases
+    dummy = ListNode(0)
+    dummy.next = head
+    current = dummy
+
+    while current.next and current.next.next:
+        # Nodes to be swapped
+        first = current.next
+        second = current.next.next
+
+        # Swapping
+        # reverse this pair
+        first.next = second.next
+        second.next = first
+        current.next = second
+
+        # Move to the next pair
+        current = current.next.next
+
+    return dummy.next
+
+# Example usage:
+# Construct a linked list: 1 -> 2 -> 3 -> 4
+head = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
+result = swapPairs(head)
+# After swapping pairs: 2 -> 1 -> 4 -> 3
