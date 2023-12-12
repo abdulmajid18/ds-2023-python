a761fbda251990428e959fa65fd345fe173d6563
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2023-12-12 07:16:24 +0000

    remove dup from sorted array

---
+++ ds/arrays/remove_duplicated_sorted_array.py
@@ -0,0 +1,54 @@
+from typing import List
+
+
+def remove_1(elements: List):
+    result = set()
+    for i in elements:
+        result.add(i)
+    print(result)
+    return len(result)
+
+def remove_4(elements):
+    l = 1
+
+    for r in range(1, len(elements)):
+        if elements[r] != elements[r-1]:
+            elements[l] = elements[r]
+            l += 1
+
+    return l + 1
+
+def remove_2(elements: List):
+    l = 1
+    for i in range(1, len(elements)):
+        if elements[i] != elements[i - 1]:
+            elements[l] = elements[i]
+            l += 1
+    return l
+
+
+def remove_3(elements: List):
+    if len(elements) < 2:
+        return len(elements)
+
+    i = 0
+    j = 1
+
+    while j < len(elements):
+        if elements[i] != elements[j]:
+            i += 1
+            elements[i] = elements[j]
+        j += 1
+    return j + 1
+
+
+
+
+
+
+if __name__ == '__main__':
+    x = [1, 1, 2]
+    ans = remove_1(x)
+    print(ans)
+    ans_2 = remove_2(x)
+    print(ans_2)
