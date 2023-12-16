bba67bdbea7549b2dd436bff16abc6cd30f8ba43
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2023-12-16 17:07:11 +0000

    updated search and insert

--- ds/binary-search/search_and_insert.py
+++ ds/binary-search/search_and_insert.py
@@ -1,4 +1,3 @@
-
 def search_and_insert(elements, target):
     left = 0
     right = len(elements) - 1
@@ -11,4 +10,19 @@ def search_and_insert(elements, target):
             left = mid + 1
     if elements[left] < target:
         return left + 1
+    return left
+
+
+def search_and_insert_2(elements, target):
+    left = 0
+    right = len(elements) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if elements[mid] == target:
+            return mid
+        if target > elements[mid]:
+            left = mid + 1
+        else:
+            left = mid - 1
     return left
