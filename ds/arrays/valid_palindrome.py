feedf6a8df4d8ce9a86a77449491cff333c21d49
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-02-07 23:51:03 +0000

    updated valid palindrome

--- ds/arrays/valid_palindrome.py
+++ ds/arrays/valid_palindrome.py
@@ -10,3 +10,20 @@ class Solution:
             if s[left].lower() != s[right].lower():
                 return False
         return True
+
+def isPalindrome(s:str):
+    l, r = 0, len(s) - 1
+
+    while l < r:
+        while l < r and not alphaNum(s[l]):
+            l += 1
+        while l < r and not alphaNum(s[r]):
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l += 1
+        r -= 1
+
+def alphaNum(c):
+    return ((ord("A") <= ord(c) <= ord("Z")) or (ord("a") <= ord(c) <= ord("z")) or
+    (ord("0") <= ord(c) <= ord("9")))
