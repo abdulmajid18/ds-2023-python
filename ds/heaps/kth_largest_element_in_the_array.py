96f027a9cb0049b71d74e72bb370b138dc07c4a1
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-05-03 21:20:37 +0000

    updated

--- ds/heaps/kth_largest_element_in_the_array.py
+++ ds/heaps/kth_largest_element_in_the_array.py
@@ -44,6 +44,49 @@ def kth_largest(nums, k):
 
 
 # Example usage:
-nums = [3, 2, 1, 5, 6, 4]
-k = 2
-print("The", k, "largest element is:", kth_largest(nums, k))
+def chunk_string(string, chunk_size):
+    """Breaks a string into chunks of specified size without splitting words."""
+    chunks = []
+    while len(string) > chunk_size:
+        # Find the index of the last occurrence of "--" before the chunk_size
+        last_hyphen_index = string.rfind('--', 0, chunk_size)
+        if last_hyphen_index == -1:
+            # If no "--" found, split at chunk_size
+            last_hyphen_index = chunk_size
+        chunks.append(string[:last_hyphen_index])
+        # Remove the processed chunk from the string
+        string = string[last_hyphen_index + 2:]  # Move beyond the "--"
+    chunks.append(string)
+    return chunks
+
+
+def send_chunks(chunks):
+    """Simulates sending each chunk."""
+    for chunk in chunks:
+        print("Sending chunk:", chunk)
+        # Replace the print statement with your actual sending mechanism
+
+new = ['brk_fee_intraday', 'ca_ircrv_intraday', 'crd_tr_intraday', 'cs_fut_intraday', 'cs_sub_intraday', 'eqd_exo_intraday', 'eqd_tr_intraday', 'gn_indx_intraday',
+       'ird_exo_intraday', 'ird_tr_intraday', 'mop_add_fl_intraday', 'mv_intraday_',
+       'mx_eqidx_intraday', 'mx_extcat_intraday', 'mx_fx_intraday', 'mx_iridx_intraday', 'mx_strats_intraday', 'pl_intraday_', 'pl_intraday_calc', 'tr_intraday', 'xg_intraday']
+# input_string = "--".join(new)
+# print(input_string)
+# chunk_size = 80
+#
+# # Break the string into chunks
+# chunks = chunk_string(input_string, chunk_size)
+#
+# # Send each chunk
+# send_chunks(chunks)
+
+import json
+
+tables = {"a": "one", "b": "two"}
+
+result = json.dumps(tables)
+result2 = str(tables)
+print(type(result))
+print(type(result2))
+print(result == result2)
+
+
