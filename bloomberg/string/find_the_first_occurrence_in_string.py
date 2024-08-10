31584a77d943bdcaae79617d82ca53cb65d12fff
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-08-10 14:48:02 +0000

    strstr

---
+++ bloomberg/string/find_the_first_occurrence_in_string.py
@@ -0,0 +1,11 @@
+def strStr1(self, haystack: str, needle: str) -> int:
+    if not needle:
+        return 0
+
+    n, m = len(haystack), len(needle)
+
+    for i in range(n - m + 1):
+        if haystack[i:i + m] == needle:
+            return i
+
+    return -1
