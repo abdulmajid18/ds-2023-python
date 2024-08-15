97791c44f5cd8f9f47274ac7a83e90fefa8c3896
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-08-15 00:41:15 +0000

    minimum substring window

--- bloomberg/dynamic_programming/general/minimum_substring_window.py
+++ bloomberg/dynamic_programming/general/minimum_substring_window.py
@@ -34,5 +34,37 @@ class Solution:
         return min_window
 
 
+class Solution2:
+    def minWindowOptimized(self, s: str, t: str) -> str:
+        if not t or not s:
+            return ""
 
+        count_t, window = {}, {}
+        for char in t:
+            count_t[char] = count_t.get(char, 0) + 1
+
+        have, need = 0, len(count_t)
+        min_len = float("inf")
+        min_window = [-1, -1]
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = window.get(c, 0) + 1
+
+            if c in count_t and window[c] == count_t[c]:
+                have += 1
+
+            while have == need:
+                if (r - l + 1) < min_len:
+                    min_window = [l, r]
+                    min_len = (r - l + 1)
+
+                # pop from left of our window
+                window[s[l]] -= 1
+                if s[l] in count_t and window[s[l]] < count_t[s[l]]:
+                    have -= 1
+                l += 1
+
+        l, r = min_window
+        return s[l:r + 1] if min_len != float("inf") else ""
 
