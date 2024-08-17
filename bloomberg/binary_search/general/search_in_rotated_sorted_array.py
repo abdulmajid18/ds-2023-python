902abc93d2b5ec6256374a492bf412db28a9e280
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-08-17 15:19:04 +0000

    search in rotated array

--- bloomberg/binary_search/general/search_in_rotated_sorted_array.py
+++ bloomberg/binary_search/general/search_in_rotated_sorted_array.py
@@ -1,5 +1,34 @@
+from typing import List
+
+
 def searchBruteForce(nums, target):
     for i in range(len(nums)):
         if nums[i] == target:
             return i
     return -1
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            # Determine the sorted portion
+            if nums[left] <= nums[mid]:  # Left portion is sorted
+                if nums[left] <= target < nums[mid]:  # Target is in the left portion
+                    right = mid - 1
+                else:  # Target is in the right portion
+                    left = mid + 1
+            else:  # Right portion is sorted
+                if nums[mid] < target <= nums[right]:  # Target is in the right portion
+                    left = mid + 1
+                else:  # Target is in the left portion
+                    right = mid - 1
+
+        return -1
+
