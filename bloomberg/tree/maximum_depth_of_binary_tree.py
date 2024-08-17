90fe5ebf4982454053ebc26e3b4f9e857c5e0937
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-08-17 20:17:22 +0000

    maximum dept: dfs, bfs and recursive

---
+++ bloomberg/tree/maximum_depth_of_binary_tree.py
@@ -0,0 +1,54 @@
+# Definition for a binary tree node.
+from collections import deque
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.right),
+                       self.maxDepth(root.left))
+
+    def maxDepthDfs(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        stack = [[root, 1]]
+        res = 1
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+
+        return res
+
+    def maxDepthBfs(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        level = 1
+        q = deque([root])
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+
+        return level
