86d6394039272ecc50e328a818bc6cc9c0a12101
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-08-25 21:36:05 +0000

    merge sorted array

--- bloomberg/two_pointers/general/merge_two_sorted_arrays.py
+++ bloomberg/two_pointers/general/merge_two_sorted_arrays.py
@@ -3,7 +3,9 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        # Start from the end of nums1 array
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
         p1 = m - 1  # Pointer for the last element in the initialized part of nums1
         p2 = n - 1  # Pointer for the last element in nums2
         p = m + n - 1  # Pointer for the last element in nums1
@@ -20,4 +22,7 @@ class Solution:
 
         # If there are remaining elements in nums2, copy them to nums1
         # No need to check nums1, because it's already in place
-        nums1[:p2 + 1] = nums2[:p2 + 1]
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p -= 1
+            p2 -= 1
