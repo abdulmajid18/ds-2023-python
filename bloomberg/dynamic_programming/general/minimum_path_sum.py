c139aee4e35ed6ad634d9b01dd156733a91517fa
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-08-26 23:30:00 +0000

    updates

--- bloomberg/dynamic_programming/general/minimum_path_sum.py
+++ bloomberg/dynamic_programming/general/minimum_path_sum.py
@@ -4,18 +4,31 @@ from typing import List
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        dp =  [[0 for n in range(n)] for i in range(m)]
+        dp = [[0 for n in range(n)] for i in range(m)]
 
         dp[0][0] = grid[0][0]
 
         for j in range(1, n):
-            dp[0][j] = grid[0][j] + dp[0][j-1]
+            dp[0][j] = grid[0][j] + dp[0][j - 1]
 
         for i in range(1, m):
-            dp[i][0] = grid[i][0] + dp[i-1][0]
+            dp[i][0] = grid[i][0] + dp[i - 1][0]
 
         for i in range(1, m):
             for j in range(1, n):
-                dp[i][j] = grid[i][j] + min(dp[i][j-1], dp[i-1][j])
+                dp[i][j] = grid[i][j] + min(dp[i][j - 1], dp[i - 1][j])
 
-        return dp[m-1][n-1]
+        return dp[m - 1][n - 1]
+
+    def minPathSum2(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        res = [[float('-inf')] * (COLS + 1) for r in range(ROWS + 1)]
+        res[ROWS - 1][COLS] = 0
+
+        for r in range(ROWS - 1, -1, -1):
+            for c in range(COLS - 1, -1, -1):
+                res[r][c] = grid[r][c] + min(grid[r + 1][c], grid[r][c + 1])
+
+        return res[0][0]
+
+
