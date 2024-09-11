0c3a1d552ccb7ebb4dcf7b5c4e3e3c9cc9ecbf57
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-09-11 21:53:16 +0000

    kth largest: quick select

--- dsa/heap/kth_largest_element_in_an_array.py
+++ dsa/heap/kth_largest_element_in_an_array.py
@@ -54,27 +54,23 @@ from typing import List
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def partition(left: int, right: int, pivot_index: int) -> int:
-            pivot_value = nums[pivot_index]
-            nums[pivot_index], nums[right] = nums[right], nums[pivot_index]
-            store_index = left
-            for i in range(left, right):
-                if nums[i] > pivot_value:
-                    nums[store_index], nums[i] = nums[i], nums[store_index]
-                    store_index += 1
-            nums[right], nums[store_index] = nums[store_index], nums[right]
-            return store_index
+        k = len(nums) - k
 
-        def quick_select(left: int, right: int, k_smallest: int) -> int:
-            if left == right:
-                return nums[left]
-            pivot_index = random.randint(left, right)
-            pivot_index = partition(left, right, pivot_index)
-            if k_smallest == pivot_index:
-                return nums[k_smallest]
-            elif k_smallest < pivot_index:
-                return quick_select(left, pivot_index - 1, k_smallest)
+        def quick_select(l, r) -> int:
+            pivot = nums[r]
+            p = l
+
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+
+            if p > k:
+                quick_select(l, p - 1)
+            elif p < k:
+                quick_select(p + 1, r)
             else:
-                return quick_select(pivot_index + 1, right, k_smallest)
+                return nums[p]
 
-        return quick_select(0, len(nums) - 1, k - 1)
+        return quick_select(0, len(nums) - 1)
