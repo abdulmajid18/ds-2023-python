775e4481a2ef66aa203c395a7568dac38b078abd
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-09-15 11:35:37 +0000

    contains duplicate i

---
+++ dsa/array/contains_duplicate.py
@@ -0,0 +1,24 @@
+def containsDuplicate(nums):
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
+
+
+def containsDuplicateOptimized(nums):
+    nums.sort()  # O(n log n)
+    for i in range(1, len(nums)):  # O(n)
+        if nums[i] == nums[i - 1]:
+            return True
+    return False
+
+
+def containsDuplicateBruteForce(nums):
+    n = len(nums)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if nums[i] == nums[j]:
+                return True
+    return False
