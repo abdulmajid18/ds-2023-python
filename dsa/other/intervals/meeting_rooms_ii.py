51286bbd2dcac38ad11f0627a26a9cca79f93117
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-09-19 09:24:01 +0000

    meeting room ii

---
+++ dsa/other/intervals/meeting_rooms_ii.py
@@ -0,0 +1,32 @@
+def minMeetingRooms(intervals):
+    if not intervals:
+        return 0
+
+    # Separate out the start and end times
+    start_times = sorted([i[0] for i in intervals])
+    end_times = sorted([i[1] for i in intervals])
+
+    # Pointers for start_times and end_times
+    start_pointer = 0
+    end_pointer = 0
+
+    # Number of rooms needed and max_rooms used at any point
+    rooms_needed = 0
+    max_rooms = 0
+
+    # Iterate over the start times
+    while start_pointer < len(intervals):
+        if start_times[start_pointer] < end_times[end_pointer]:
+            # A meeting is starting, so we need a new room
+            rooms_needed += 1
+            start_pointer += 1
+        else:
+            # A meeting ended, so we free up a room
+            rooms_needed -= 1
+            end_pointer += 1
+
+        # Track the maximum number of rooms needed at any point
+        max_rooms = max(max_rooms, rooms_needed)
+
+    return max_rooms
+
