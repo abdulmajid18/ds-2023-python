f90194be5ea6b6f636bd136de31d6b2be1a82e09
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-09-19 09:35:30 +0000

    meeting room i sweep line

--- dsa/other/intervals/meeting_rooms_i.py
+++ dsa/other/intervals/meeting_rooms_i.py
@@ -15,3 +15,27 @@ def canAttendMeetings2(intervals):
         if i1[1] > i2[0]:
             return False
     return True
+
+
+def minMeetingRooms(intervals):
+    events = []
+
+    # Create a list of start and end events
+    for interval in intervals:
+        events.append((interval[0], 1))  # Meeting starts
+        events.append((interval[1], -1))  # Meeting ends
+
+    # Sort events by time. If times are the same, process end (-1) first
+    events.sort(key=lambda x: (x[0], x[1]))
+
+    count = 0
+    max_rooms = 0
+
+    # Process the events
+    for event in events:
+        count += event[1]  # Increment or decrement the count
+
+        # Track the maximum number of rooms needed
+        max_rooms = max(max_rooms, count)
+
+    return max_rooms
