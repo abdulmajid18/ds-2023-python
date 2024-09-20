ca65ef8fe80fbde77e6b9415624dc631c3fc9d06
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-09-20 08:36:53 +0000

    path sum

--- dsa/tree/binary_tree_path_sum.py
+++ dsa/tree/binary_tree_path_sum.py
@@ -26,3 +26,34 @@ class Solution:
 
         if root.right:
             self.dfsHelper(root.right, tempPath + str(root.val) + "->", res)
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def binaryTreePaths(self, root: TreeNode):
+        result = []
+
+        def dfs(node, path):
+            if node:
+                # Add the current node's value to the path
+                path += str(node.val)
+
+                # If it's a leaf node, append the path to the result
+                if not node.left and not node.right:
+                    result.append(path)
+                else:
+                    # If not a leaf, continue DFS on children with updated path
+                    path += "->"  # Add arrow for the next node in the path
+                    dfs(node.left, path)
+                    dfs(node.right, path)
+
+        # Start DFS from the root
+        dfs(root, "")
+        return result
