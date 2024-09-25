8a32a38e29751b0c23e96a35c8d068fc9c64cd84
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-09-25 09:40:30 +0000

    top k frequent element bucket sort

--- dsa/heap/top_k_frequent_elements.py
+++ dsa/heap/top_k_frequent_elements.py
@@ -18,3 +18,23 @@ class Solution:
             result.append(largest[1])
 
         return result
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        counter = {}
+        for num in nums:
+            counter[num] = counter.get(num, 0) + 1
+
+        bucket = [[] for _ in range(len(nums) + 1)]
+
+        for num, freq in counter.items():
+            bucket[freq].append(num)
+
+        res = []
+        for i in range(len(bucket) - 1, -1, -1):
+            for n in bucket[i]:
+                res.append(n)
+            if len(res) == k:
+                return res
