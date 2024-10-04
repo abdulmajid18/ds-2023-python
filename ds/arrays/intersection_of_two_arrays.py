5f31483d109d6e4178fdc135927e5e15988a226b
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-10-04 00:03:31 +0000

    intersection two pointers

--- ds/arrays/intersection_of_two_arrays.py
+++ ds/arrays/intersection_of_two_arrays.py
@@ -21,3 +21,25 @@ class Solution:
                 res.append(n)
                 nums1_set.remove(n)
         return res
+
+
+def intersectionTwoPointers(nums1, nums2):
+    # Sort both arrays
+    nums1.sort()
+    nums2.sort()
+
+    i, j = 0, 0
+    result = set()
+
+    # Use two pointers to find common elements
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] == nums2[j]:
+            result.add(nums1[i])
+            i += 1
+            j += 1
+        elif nums1[i] < nums2[j]:
+            i += 1
+        else:
+            j += 1
+
+    return list(result)
