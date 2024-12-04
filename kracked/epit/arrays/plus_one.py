367130a5cdba6bad1eaf588ec82bb7d2ff32ba97
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-12-04 12:31:42 +0000

    epit: array plus one

---
+++ kracked/epit/arrays/plus_one.py
@@ -0,0 +1,24 @@
+def plus_one(A):
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i - 1] += 1
+    if A[0] == 10:
+        A[0] = 1
+        A.append(0)
+    return A
+
+
+def plus_one_2(A):
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i - 1] += 1
+    if A[0] == 10:
+        A[0] = 1
+        A.insert(0, 1)
+    return A
