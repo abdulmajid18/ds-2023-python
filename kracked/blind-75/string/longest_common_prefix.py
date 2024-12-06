fdd854b20bd1738217c6a8e76c4534a1a3bbcc71
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-12-06 12:12:59 +0000

    grind169: longest common prefix

---
+++ kracked/blind-75/string/longest_common_prefix.py
@@ -0,0 +1,8 @@
+def longest_common_prefix(strs):
+    res = ""
+    for i in range(len(strs[0])):
+        for s in strs:
+            if i == len(s) or strs[0][i] != s[i]:
+                return res
+            res += strs[0][i]
+    return res
