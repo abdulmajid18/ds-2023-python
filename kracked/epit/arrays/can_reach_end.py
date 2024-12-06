5d996d3083a826cc6a513ee09aedf4879f4e1bf4
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-12-06 14:58:24 +0000

    epit: jump game

--- kracked/epit/arrays/can_reach_end.py
+++ kracked/epit/arrays/can_reach_end.py
@@ -19,3 +19,14 @@ def can_reach_end(A):
             return True
 
     return False  # If we finish the loop without reaching the end
+
+
+def can_reach_end2(A):
+    furthest_reach_so_far, last_index = 0, len(A) - 1
+    i = 0
+
+    while i <= furthest_reach_so_far and furthest_reach_so_far < last_index:
+        furthest_reach_so_far = max(furthest_reach_so_far, A[i] + i)
+        i += 1
+
+    return furthest_reach_so_far >= last_index
