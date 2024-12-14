ab04bc2a1c42742576882cec16b4f99afafb3c7f
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-12-14 14:01:37 +0000

    grind169: updated three sum

--- kracked/blind-75/two_pointers/three_sum.py
+++ kracked/blind-75/two_pointers/three_sum.py
@@ -3,10 +3,10 @@ def three_sum(nums):
     nums.sort()
 
     for i, v in enumerate(nums):
-        if i > 0 and v == nums[i-1]:
+        if i > 0 and v == nums[i - 1]:
             continue
 
-        l, r = i + 1, len(nums)-1
+        l, r = i + 1, len(nums) - 1
         while l < r:
             threeSum = v + nums[l] + nums[r]
             if threeSum > 0:
@@ -15,3 +15,8 @@ def three_sum(nums):
                 l += 1
             else:
                 res.append([v, nums[l], nums[r]])
+                l += 1
+                while nums[l] == nums[l - 1] and l < r:
+                    l += 1
+
+    return res
