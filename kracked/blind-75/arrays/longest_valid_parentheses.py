0e620742d861f0766cb65b67ee092519e85614b3
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-01-21 00:21:15 +0000

    grind169: longest_valid_parentheses two pass

--- kracked/blind-75/arrays/longest_valid_parentheses.py
+++ kracked/blind-75/arrays/longest_valid_parentheses.py
@@ -13,4 +13,35 @@ def longest_valid_parentheses(s):
                 max_length = max(max_length, i - left_par_idx)
     return max_length
 
+
 # a = "(()"
+
+def longestValidParenthesesTwoPass(s: str) -> int:
+    # Using two passes
+    max_length = 0
+    open_count, close_count = 0, 0
+
+    # Left to right
+    for char in s:
+        if char == '(':
+            open_count += 1
+        else:
+            close_count += 1
+        if open_count == close_count:
+            max_length = max(max_length, 2 * close_count)
+        elif close_count > open_count:
+            open_count = close_count = 0
+
+    # Right to left
+    open_count, close_count = 0, 0
+    for char in reversed(s):
+        if char == '(':
+            open_count += 1
+        else:
+            close_count += 1
+        if open_count == close_count:
+            max_length = max(max_length, 2 * open_count)
+        elif open_count > close_count:
+            open_count = close_count = 0
+
+    return max_length
