71f36386d3612f104e52c76450a41a5cea3c5ce1
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-01-23 23:54:53 +0000

    meeting rooms: 1

---
+++ kracked/blind-75/intervals/meeting_room_i.py
@@ -0,0 +1,18 @@
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+
+def canAttendMeeting(intervals):
+    intervals.sort(key=lambda i: i.start)
+
+    # prev
+    for i in range(1, len(intervals)):
+        i1 = intervals[i - 1]
+        i2 = intervals[i]
+
+        if i1.end > i2.start:
+            return False
+
+    return True
