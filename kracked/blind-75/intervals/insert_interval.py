ec8c90458796945d071fe2d1d9c2bd8c108b46b3
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-01-27 00:56:18 +0000

    insert interval using arrays

--- kracked/blind-75/intervals/insert_interval.py
+++ kracked/blind-75/intervals/insert_interval.py
@@ -1,5 +1,6 @@
 import heapq
 
+
 def insert_with_priority_queue(intervals, newInterval):
     # Step 1: Add all intervals and newInterval into the heap
     heap = intervals + [newInterval]
@@ -16,3 +17,18 @@ def insert_with_priority_queue(intervals, newInterval):
             result[-1][1] = max(result[-1][1], interval[1])
 
     return result
+
+
+def insert_interval(intervals, newInterval):
+    res = []
+
+    for i in range(len(intervals)):
+        if newInterval[1] < intervals[i][0]:
+            res.append(newInterval)
+            return res + intervals[i:]
+        elif newInterval[0] > intervals[i][1]:
+            res.append((intervals[i]))
+        else:
+            newInterval = [min(newInterval[0], intervals[i][0], max(newInterval[1], intervals[i][1]))]
+    res.append(newInterval)
+    return res
