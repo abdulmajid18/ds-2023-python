cc32fa422daabd6954e1ba260af3f8039c6e9dee
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-02-02 23:51:04 +0000

    sliding windows

--- kracked/blind-75/sliding_window/frequency_of_the_mosy_frequent_element.py
+++ kracked/blind-75/sliding_window/frequency_of_the_mosy_frequent_element.py
@@ -14,3 +14,23 @@ def maxFrequency(nums, k):
         max_freq = max(max_freq, right - left + 1)
 
     return max_freq
+
+
+def maxFrequency2(nums, k):
+    nums.sort()
+    left = 0
+    max_freq = 0
+    current_cost = 0
+
+    for right in range(len(nums)):
+        if right > 0:
+            current_cost += (nums[right] - nums[right - 1]) * (right - left)
+
+        while current_cost > k:
+            current_cost -= nums[right] - nums[left]
+            left += 1
+
+        max_freq = max(max_freq, right - left + 1)
+
+    return max_freq
+
