72160536d9b7e670ac50e879245f33e2db9fa20e
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-02-16 18:46:58 +0000

    graphs: dfs iterative

--- 2025/graphs/basics/dfs.py
+++ 2025/graphs/basics/dfs.py
@@ -23,3 +23,19 @@ graph = {
 # Run DFS
 print("DFS Recursive Traversal:")
 dfs_recursive(graph, 'A')
+
+
+def dfs_iterative(graph, start):
+    visited = set()
+    stack = [start]
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            print(node, end=" ")  # Process the node
+            visited.add(node)
+            stack.extend(reversed(graph.get(node, [])))  # Reverse to maintain correct order
+
+# Run Iterative DFS
+print("\nDFS Iterative Traversal:")
+dfs_iterative(graph, 'A')
