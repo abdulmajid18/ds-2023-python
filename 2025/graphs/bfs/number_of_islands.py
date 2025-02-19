3b17830493deb6ddb9b15e7d13b69d13d6f608da
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-02-19 01:39:25 +0000

    graphs: number of islands dfs

--- 2025/graphs/bfs/number_of_islands.py
+++ 2025/graphs/bfs/number_of_islands.py
@@ -67,3 +67,32 @@ class Solution:
                     islands += 1
         return islands
 
+
+from typing import List
+
+
+class SolutionDFS:
+    def numIslandsDFS(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        num_islands = 0
+
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == '0':
+                return
+
+            grid[r][c] = '0'  # Mark as visited
+            dfs(r + 1, c)  # Down
+            dfs(r - 1, c)  # Up
+            dfs(r, c + 1)  # Right
+            dfs(r, c - 1)  # Left
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':  # Found an unvisited land cell
+                    num_islands += 1
+                    dfs(r, c)
+
+        return num_islands
