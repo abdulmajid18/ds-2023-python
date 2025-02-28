78fa3447d207c092ad2f6368113a052ddaa60ce1
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-02-28 23:16:12 +0000

    search in 2d matrix

---
+++ 2025/binary_search/one_dimension/search_2d_matrix.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+def searchMatrixBruteForce(matrix, target):
+    if not matrix or not matrix[0]:
+        return False
+
+    for row in matrix:
+        for element in row:
+            if element == target:
+                return True
+
+    return False
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            mid_value = matrix[mid // n][mid % n]
+
+            if mid_value == target:
+                return True
+            elif mid_value < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
+
+
