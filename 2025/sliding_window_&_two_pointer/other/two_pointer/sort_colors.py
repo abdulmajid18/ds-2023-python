daacca25fcdd3f0bfdf28250e1b5bc9ab12c5f7d
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-03-31 22:25:11 +0000

    quick sort

--- 2025/sliding_window_&_two_pointer/other/two_pointer/sort_colors.py
+++ 2025/sliding_window_&_two_pointer/other/two_pointer/sort_colors.py
@@ -37,3 +37,18 @@ nums = [2, 0, 2, 1, 1, 0]
 sol = SolutionBruteForce()
 sol.sortColors(nums)
 print(nums)  # Output: [0, 0, 1, 1, 2, 2]
+
+
+def sortColors(nums):
+    left, mid, right = 0, 0, len(nums) - 1
+
+    while mid <= right:
+        if nums[mid] == 0:
+            nums[left], nums[mid] = nums[mid], nums[left]
+            left += 1
+            mid += 1
+        elif nums[mid] == 1:
+            mid += 1
+        else:  # nums[mid] == 2
+            nums[mid], nums[right] = nums[right], nums[mid]
+            right -= 1
