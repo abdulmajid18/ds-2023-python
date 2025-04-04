9cc7bdab281633c2c763fc4046c34b94e6408b3e
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-04-04 20:31:18 +0000

    linked list

---
+++ 2025/other/amz/linked_list/remove_duplicate_from_linked_list_ii.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def deleteDuplicates(head: Optional[ListNode]) -> Optional[ListNode]:
+    dummy = ListNode(next=head)
+    prev = dummy
+
+    while head:
+        if head.next and head.val == head.next.val:
+            while head.next and head.val == head.next.val:
+                head = head.next
+            prev.next = head.next
+        else:
+            prev = prev.next
+        head = head.next
+    return dummy.next
