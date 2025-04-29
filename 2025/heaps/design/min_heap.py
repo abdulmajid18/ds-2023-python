501613b8569d8a82e481774b4fb5fba07ae248d1
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-04-29 02:06:59 +0000

    min heap impl

---
+++ 2025/heaps/design/min_heap.py
@@ -0,0 +1,44 @@
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def push(self, val):
+        self.heap.append(val)
+        self._heapify_up(len(self.heap) - 1)
+
+    def pop(self):
+        if not self.heap:
+            return None
+        self._swap(0, len(self.heap) - 1)
+        val = self.heap.pop()
+        self._heapify_down(0)
+        return val
+
+    def peek(self):
+        if not self.heap:
+            return None
+        return self.heap[0]
+
+    def _heapify_up(self, index):
+        parent = (index - 1) // 2
+        if index > 0 and self.heap[index] < self.heap[parent]:
+            self._swap(index, parent)
+            self._heapify_up(parent)
+
+    def _heapify_down(self, index):
+        smallest = index
+        left = 2 * index + 1
+        right = 2 * index + 2
+
+        if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
+            smallest = left
+
+        if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+
+        if smallest != index:
+            self._swap(index, smallest)
+            self._heapify_down(smallest)
+
+    def _swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
