a585009446b4d4a770a55fe6a6104f3965a706bd
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2025-05-03 23:18:02 +0000

    sliding window: prefix sum

--- 2025/sliding_window_&_two_pointer/medium/binary_array_sum.py
+++ 2025/sliding_window_&_two_pointer/medium/binary_array_sum.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def numSubarraysWithSumBrute(nums: list[int], goal: int) -> int:
     count = 0
     n = len(nums)
@@ -32,3 +35,32 @@ def numSubarraysWithSum(nums: list[int], goal: int) -> int:
         prefix_count[prefix_sum] += 1
 
     return result
+
+
+def numSubarraysWithSumTwo(self, nums: List[int], goal: int) -> int:
+    seen = defaultdict(int)
+    seen[0] = 1  # empty prefix
+    pref = ans = 0
+
+    for x in nums:
+        pref += x
+        ans += seen[pref - goal]  # how many previous prefixes give sum=goal
+        seen[pref] += 1
+
+    return ans
+
+
+def numSubarraysWithSumTwoPointer(nums, goal):
+    def atMost(k):
+        if k < 0:
+            return 0
+        left = total = 0
+        for right, val in enumerate(nums):
+            k -= val
+            while k < 0:
+                k += nums[left]
+                left += 1
+            total += right - left + 1
+        return total
+
+    return atMost(goal) - atMost(goal - 1)
