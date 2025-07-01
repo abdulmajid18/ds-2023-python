ba0594a23f9ae89afc5a00e2d13e7bdec732a67c
Abdul-Majid Nuhu <nuhuabdulmajid58@gmail.com>
2025-07-01 10:40:38 +0000

    prefix: k subarray sum optimized

--- 2025/coding_interview_patterns/prefix_sums/sub_array_sum_equals_k.py
+++ 2025/coding_interview_patterns/prefix_sums/sub_array_sum_equals_k.py
@@ -46,3 +46,34 @@ def subarraySumThree(nums: List[int], k: int) -> int:
     return count
 
 
+from collections import defaultdict
+
+def subarraySumFourOptimizes(nums: List[int], k: int) -> int:
+    count = 0
+    prefix_sum = 0
+    prefix_map = defaultdict(int)
+    prefix_map[0] = 1  # base case: prefix_sum - k == 0
+
+    for num in nums:
+        prefix_sum += num
+        count += prefix_map[prefix_sum - k]
+        prefix_map[prefix_sum] += 1
+
+    return count
+
+
+def k_sum_subarrays_optimized(nums: List[int], k: int) -> int:
+    count = 0
+    prefix_sum_map = {0: 1}
+    curr_prefix_sum = 0
+
+    for num in nums:
+        curr_prefix_sum += num
+
+        if (curr_prefix_sum - k) in prefix_sum_map:
+            count += prefix_sum_map[curr_prefix_sum - k]
+
+        freq = prefix_sum_map.get(curr_prefix_sum, 0)
+        prefix_sum_map[curr_prefix_sum] = freq + 1
+
+    return count
