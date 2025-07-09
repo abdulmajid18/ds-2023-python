2e2bc1dc82b904d454446e52563b04ad8a01575a
Abdul-Majid Nuhu <nuhuabdulmajid58@gmail.com>
2025-07-09 22:53:16 +0000

    graph: bfs

--- 2025/graphs/bfs/bfs_ajacency_list.py
+++ 2025/graphs/bfs/bfs_ajacency_list.py
@@ -29,3 +29,49 @@ graph = {
 
 # Start BFS from node 'A'
 bfs_adjacency_list(graph, 'A')
+
+from collections import deque
+
+
+def bfs_one(graph, root):
+    visited = set()
+    queue = deque([root])
+    res = []
+    while queue:
+        qLen = len(queue)
+
+        for i in range(qLen):
+            current_node = queue.popleft()
+            if current_node not in visited:
+                visited.add(current_node)
+                res.append(current_node)
+            for new_node in graph[current_node]:
+                if new_node not in visited:
+                    queue.append(new_node)
+    return res
+
+def bfs_two(graph, root):
+    visited = set()
+    queue = deque([root])
+    while queue:
+        vertex = queue.popleft()
+        visited.add(vertex)
+        for v in graph[vertex]:
+            if v not in visited:
+                queue.append(v)
+    print(visited)
+    return visited
+if __name__ == "__main__":
+
+    graph = {
+        0: [1,2,3],
+        1: [0,2],
+        2:[0,1],
+        3: [0],
+        4: [2]
+    }
+
+    res = bfs_one(graph, 0)
+    print(res)
+    res_2 = bfs_two(graph, 0)
+    print(res_2)
