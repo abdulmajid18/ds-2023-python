d9a8cdef0cb11faefb7e3498a949656ffac8e520
Abdul-Majid Nuhu <nuhuabdulmajid58@gmail.com>
2025-07-26 21:24:39 +0000

    two pointers

--- cip/two_pointers/container_with_most_water.py
+++ cip/two_pointers/container_with_most_water.py
@@ -36,3 +36,24 @@ def maxAreaOptimized(height: List[int]) -> int:
 
     return max_area
 
+
+from typing import List
+
+
+def largest_container(heights: List[int]) -> int:
+    max_water = 0
+    left, right = 0, len(heights) - 1
+
+    while left < right:
+        water = min(heights[left], heights[right]) * (right - left)
+        max_water = max(max_water, water)
+
+        if heights[left] < heights[right]:
+            left += 1
+        elif heights[left] > heights[right]:
+            right -= 1
+        else:
+            left += 1
+            right -= 1
+
+    return max_water
