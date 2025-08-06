49741617479c3f143f315604bbc6646cff27316c
Abdul-Majid Nuhu <nuhuabdulmajid58@gmail.com>
2025-08-06 09:56:06 +0000

    binary search: minimum in rotated sorted array

--- cip/binary_search/find_the_minimum_in_rotated_sorted_array.py
+++ cip/binary_search/find_the_minimum_in_rotated_sorted_array.py
@@ -1,13 +1,23 @@
 from typing import List
 
 
-class Solution:
-    def findMin(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
-        while l < r:
-            mid = (l + r) // 2
-            if nums[mid] > nums[r]:
-                l = mid + 1
-            else:
-                r = mid
-        return nums[l]
+def findMin(nums: List[int]) -> int:
+    l, r = 0, len(nums) - 1
+    while l < r:
+        mid = (l + r) // 2
+        if nums[mid] > nums[r]:
+            l = mid + 1
+        else:
+            r = mid
+    return nums[l]
+
+
+def findMinTwo(nums):
+    i, j = 0, len(nums) - 1
+    while i < j:
+        mid = (i + j) // 2
+        if nums[mid] >= nums[0]:
+            i = mid + 1
+        else:
+            j = mid
+    return nums[i]
