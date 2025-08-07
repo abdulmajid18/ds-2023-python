959b0789a79889a40e47cad92cd16d94771ef482
Abdul-Majid Nuhu <nuhuabdulmajid58@gmail.com>
2025-08-07 14:42:07 +0000

    update

--- cip/dp/1D/climbing_stairs.py
+++ cip/dp/1D/climbing_stairs.py
@@ -11,3 +11,29 @@ def climbStairs(n: int) -> int:
     memo = {}
 
     return climb(n)
+
+
+def climbStairsTwo(n: int) -> int:
+    if n <= 2:
+        return n
+
+    first, second = 1, 2
+    for i in range(3, n + 1):
+        third = first + second
+        first = second
+        second = third
+    return second
+
+
+def climbStairsDP(n: int) -> int:
+    if n <= 2:
+        return n
+
+    dp = [0] * (n + 1)
+    dp[0] = 1  # 1 way to be at step 0 (do nothing)
+    dp[1] = 1  # 1 way to reach step 1 (0 → 1)
+
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
